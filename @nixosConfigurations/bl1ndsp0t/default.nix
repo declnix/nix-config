c5b9221f0566fe87870bb5d4e4a5b37dc58fbfd9
declnix <20567435+declnix@users.noreply.github.com>
2026-01-11 01:06:09 +0100

    first commit

---
+++ @nixosConfigurations/bl1ndsp0t/default.nix
@@ -0,0 +1,114 @@
+{
+  system = "x86_64-linux";
+
+  modules = [
+    {
+      networking.hostName = "bl1ndsp0t";
+      i18n.defaultLocale = "en_US.UTF-8";
+    }
+
+    (
+      { pkgs, ... }:
+      {
+        users.users.declnix = {
+          isNormalUser = true;
+          home = "/home/declnix";
+          extraGroups = [
+            "wheel"
+            "networkmanager"
+            "libvirtd"
+            "kvm"
+          ];
+
+          initialPassword = "password";
+          ignoreShellProgramCheck = true;
+        };
+      }
+    )
+
+    (
+      { inputs, pkgs, ... }:
+      {
+        users.users.declnix = {
+          packages =
+            (with inputs.self.packages.${pkgs.system}; [
+              (zsh.apply {
+                aliases = {
+                  rebuild = "sudo nixos-rebuild switch --flake .";
+                  gs = "git status";
+                };
+              })
+
+              tmux
+            ])
+            ++ (with pkgs; [
+              git
+              wget
+              curl
+              fzf
+              eza
+              bat
+
+              # ai
+              claude-code
+
+              # gui / browsers
+              firefox
+            ]);
+        };
+      }
+    )
+
+    (
+      { pkgs, ... }:
+      {
+        fonts.packages = with pkgs; [ nerd-fonts.fira-code ];
+      }
+    )
+
+    (
+      { pkgs, ... }:
+      {
+        environment.systemPackages = with pkgs; [
+          vim
+        ];
+      }
+    )
+
+    {
+      virtualisation.libvirtd.enable = true;
+      programs.virt-manager.enable = true;
+    }
+
+    {
+      services = {
+        desktopManager.plasma6.enable = true;
+        displayManager.sddm.enable = true;
+        displayManager.sddm.wayland.enable = true;
+      };
+    }
+
+    {
+      security.sudo.enable = true;
+      security.sudo.wheelNeedsPassword = false;
+    }
+
+    {
+      virtualisation.vmVariant = {
+        users.users.declnix.password = "password";
+        security.sudo.wheelNeedsPassword = false;
+      };
+    }
+
+    {
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+
+      nixpkgs.config.allowUnfree = true;
+    }
+
+    ./configuration.nix
+  ];
+}
