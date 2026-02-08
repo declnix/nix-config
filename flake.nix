4d9ca9ae06af45e3356259e76339e1a94a09d08f
declnix <20567435+declnix@users.noreply.github.com>
2026-02-08 23:10:31 +0000

    feat(core): add home-manager integration

--- flake.nix
+++ flake.nix
@@ -1,14 +1,35 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flakelight.url = "github:nix-community/flakelight";
-    wrappers.url = "github:lassulus/wrappers";
-    nzf.url = "github:yehvaed/nzf";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+    # ===========================================================
+    # CHANNELS
+    # ===========================================================
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # ===========================================================
+    # PLATFORMS
+    # ===========================================================   
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+
+    # ===========================================================
+    # FRAMEWORKS
+    # ===========================================================   
+    flakelight = {
+      url = "github:nix-community/flakelight";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    wrappers.url = "github:lassulus/wrappers";
+
+    # ===========================================================
+    # APPLICATIONS
+    # ===========================================================
+    nzf.url = "github:yehvaed/nzf";
   };
 
   outputs = inputs@{ flakelight, ... }: (flakelight ./.) (import ./outputs.nix inputs);
