86365eeb7cd7947730a26a99621e50f46461ceb9
declnix <20567435+declnix@users.noreply.github.com>
2026-02-18 11:37:52 +0000

    refactor(c4rg0x): consolidate programs config in home module

--- @nixosConfigurations/c4rg0x/home.nix
+++ @nixosConfigurations/c4rg0x/home.nix
@@ -1,28 +1,31 @@
 { pkgs, ... }: {
-  programs.git = {
-    enable = true;
-    extraConfig = {
-      credential.helper =
-        "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
+  programs = {
+    git = {
+      enable = true;
+      extraConfig = {
+        credential.helper =
+          "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe";
+      };
     };
-  };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-  };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+    };
 
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
 
-  programs.fzf.enable = true;
-  programs.ripgrep.enable = true;
-  programs.bat.enable = true;
-  programs.eza = {
-    enable = true;
-    enableZshIntegration = true;
+    fzf.enable = true;
+    ripgrep.enable = true;
+    bat.enable = true;
+
+    eza = {
+      enable = true;
+      enableZshIntegration = true;
+    };
   };
 
   home.packages = with pkgs; [ devbox claude-code ];
