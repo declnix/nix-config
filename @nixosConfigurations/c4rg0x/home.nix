dd81cc34edc73b85bf1d5cf640b04ae3b1af8201
declnix <20567435+declnix@users.noreply.github.com>
2026-02-20 23:59:53 +0000

    feat(c4rg0x): enable zsh autosuggestion and syntax highlighting

--- @nixosConfigurations/c4rg0x/home.nix
+++ @nixosConfigurations/c4rg0x/home.nix
@@ -11,6 +11,10 @@
     zsh = {
       enable = true;
       enableCompletion = true;
+      
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      
       oh-my-zsh = {
         enable = true;
         plugins = [ "git" ];
