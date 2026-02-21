2215858c34c72d6453413b15e76f24dd66db93d8
declnix <20567435+declnix@users.noreply.github.com>
2026-02-21 07:27:49 +0000

    feat(pstnd01): add zscaler certificate

--- @nixosConfigurations/pstnd01/configuration.nix
+++ @nixosConfigurations/pstnd01/configuration.nix
@@ -5,7 +5,7 @@
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [
@@ -20,6 +20,8 @@
   };
 
   programs.nix-ld.enable = true;
+
+  security.pki.certificateFiles = [ "${pkgs.zscaler-cacert}/etc/ssl/certs/zscaler-ca.crt" ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
