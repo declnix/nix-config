6de956fce257b398967ed89dee47a1992b2ed099
declnix <20567435+declnix@users.noreply.github.com>
2026-02-21 07:27:52 +0000

    feat(pstnd01): enable zsh autosuggestion and syntax highlighting

--- @nixosConfigurations/pstnd01/default.nix
+++ @nixosConfigurations/pstnd01/default.nix
@@ -24,6 +24,30 @@
             programs.zsh = {
               enable = true;
               enableCompletion = true;
+
+              autosuggestion.enable = true;
+              syntaxHighlighting.enable = true;
+
+              oh-my-zsh = {
+                enable = true;
+                plugins = [ "git" ];
+                theme = "robbyrussell";
+              };
+              plugins = [
+                {
+                  name = "zsh-fzf-history-search";
+                  src = pkgs.zsh-fzf-history-search;
+                }
+                {
+                  name = "fzf-tab";
+                  src = pkgs.fetchFromGitHub {
+                    owner = "Aloxaf";
+                    repo = "fzf-tab";
+                    rev = "v1.2.0";
+                    sha256 = "sha256-q26XVS/LcyZPRqDNwKKA9exgBByE0muyuNb0Bbar2lY=";
+                  };
+                }
+              ];
             };
 
             programs.direnv = {
