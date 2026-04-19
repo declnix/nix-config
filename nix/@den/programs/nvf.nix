f371f78a7667409001d49aa3dc96959584b46510
declnix <20567435+declnix@users.noreply.github.com>
2026-04-19 22:00:21 +0200

    feat(nvf): smart neotree toggle, quit hook, and fzflua keymaps binding

--- nix/@den/programs/nvf.nix
+++ nix/@den/programs/nvf.nix
@@ -54,6 +54,39 @@
       };
 
       fzf-lua.enable = true;
+      luaConfigPost = ''
+        vim.api.nvim_create_autocmd("QuitPre", {
+          callback = function()
+            local wins = vim.tbl_filter(function(win)
+              return vim.api.nvim_win_is_valid(win)
+                and vim.api.nvim_win_get_config(win).relative == ""
+                and vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "neo-tree"
+            end, vim.api.nvim_list_wins())
+            if #wins == 1 then
+              vim.cmd("Neotree close")
+            end
+          end,
+        })
+
+        function _toggle_neotree()
+          local cur = vim.api.nvim_get_current_win()
+          local nt_open = false
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            if vim.api.nvim_win_is_valid(win) and vim.bo[vim.api.nvim_win_get_buf(win)].filetype == "neo-tree" then
+              nt_open = true
+              break
+            end
+          end
+          if nt_open then
+            vim.cmd("Neotree close")
+            if vim.api.nvim_win_is_valid(cur) and vim.bo[vim.api.nvim_win_get_buf(cur)].filetype ~= "neo-tree" then
+              vim.api.nvim_set_current_win(cur)
+            end
+          else
+            vim.cmd("Neotree focus")
+          end
+        end
+      '';
       maps.normal = {
         "<leader>ff" = {
           action = "<cmd>FzfLua files<CR>";
@@ -63,8 +96,12 @@
           action = "<cmd>FzfLua live_grep<CR>";
           desc = "Search in files";
         };
+        "<leader>fk" = {
+          action = "<cmd>FzfLua keymaps<CR>";
+          desc = "Find keymaps";
+        };
         "<leader>e" = {
-          action = ":Neotree toggle<CR>";
+          action = "<cmd>lua _toggle_neotree()<CR>";
           desc = "Toggle file explorer";
         };
       };
