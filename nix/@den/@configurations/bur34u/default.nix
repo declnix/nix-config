a24fba7f4c686e3e271b618969a9ec7f13a9feeb
declnix <20567435+declnix@users.noreply.github.com>
2026-04-19 22:00:21 +0200

    refactor(den): rename host/user files to default.nix, merge wsl hosts into single file

--- nix/@den/@configurations/bur34u/default.nix
+++ nix/@den/@configurations/bur34u/default.nix
@@ -15,4 +15,12 @@
     };
 
   den.hosts.x86_64-linux.bur34u.users.nixos = { };
+
+  den.aspects.bur34u =
+    { host, ... }:
+    {
+      includes = with den.aspects; [ wsl ];
+    };
+
+  den.hosts.x86_64-linux.bur34u = { };
 }
--- nix/@den/@configurations/c4rg0x/default.nix
+++ nix/@den/@configurations/c4rg0x/default.nix
@@ -1,6 +1,5 @@
 { den, ... }:
 {
-
   den.aspects.nixos.provides.c4rg0x =
     { ... }:
     {
@@ -19,4 +18,15 @@
     };
 
   den.hosts.x86_64-linux.c4rg0x.users.nixos = { };
+
+  den.aspects.c4rg0x =
+    { host, ... }:
+    {
+      includes = with den.aspects; [
+        wsl
+        zscaler
+      ];
+    };
+
+  den.hosts.x86_64-linux.c4rg0x = { };
 }
