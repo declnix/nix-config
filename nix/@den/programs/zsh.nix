4670e898acceec0fe3076e8891a67073d5c278b1
declnix <20567435+declnix@users.noreply.github.com>
2026-04-19 22:00:21 +0200

    fix(zsh): show user@host in prompt over ssh

--- nix/@den/programs/zsh.nix
+++ nix/@den/programs/zsh.nix
@@ -21,7 +21,11 @@
             source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
             source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
             source ${pkgs.zsh-fzf-history-search}/share/zsh-fzf-history-search/zsh-fzf-history-search.plugin.zsh
-            PROMPT="%B%F{magenta}❯%f%b "
+            if [[ -n $SSH_CLIENT ]]; then
+              PROMPT="%F{green}%n@%m%f %B%F{magenta}❯%f%b "
+            else
+              PROMPT="%B%F{magenta}❯%f%b "
+            fi
           '';
         };
       };
