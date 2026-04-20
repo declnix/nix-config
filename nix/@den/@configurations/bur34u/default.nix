6a0b64e3e3408554297e7ea1e27956dde6772b8d
declnix <20567435+declnix@users.noreply.github.com>
2026-04-20 07:34:23 +0200

    feat(bur34u): add declnix user

--- nix/@den/@configurations/bur34u/default.nix
+++ nix/@den/@configurations/bur34u/default.nix
@@ -16,6 +16,22 @@
 
   den.hosts.x86_64-linux.bur34u.users.nixos = { };
 
+  den.aspects.declnix.provides.bur34u =
+    { ... }:
+    {
+      nixos = {
+        users.users.declnix.initialPassword = "test";
+      };
+
+      includes = [
+        den._.primary-user
+        (den._.user-shell "zsh")
+      ]
+      ++ (with den.aspects; [ dev-tools ]);
+    };
+
+  den.hosts.x86_64-linux.bur34u.users.declnix = { };
+
   den.aspects.bur34u =
     { host, ... }:
     {
