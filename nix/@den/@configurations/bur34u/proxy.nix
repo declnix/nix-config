c103fa2eb15378a6393d89b4c8220e007c2f07df
declnix <20567435+declnix@users.noreply.github.com>
2026-04-21 10:01:37 +0000

    fix(bur34u): source proxy env from zshenv instead of zshrc
    Move proxy env sourcing from rum initConfig (.zshrc) to
    programs.zsh.shellInit (/etc/zshenv) so variables are available
    in all shells immediately after boot.

--- nix/@den/@configurations/bur34u/proxy.nix
+++ nix/@den/@configurations/bur34u/proxy.nix
@@ -1,40 +1,34 @@
 { den, ... }:
 {
-  den.aspects.bur34u = {
-    nixos =
-      { pkgs, ... }:
-      {
-        systemd.services.nix-proxy-env = {
-          wantedBy = [ "nix-daemon.service" ];
-          before = [ "nix-daemon.service" ];
-          path = [
-            pkgs.iproute2
-            pkgs.gawk
-          ];
-          serviceConfig = {
-            Type = "oneshot";
-            RemainAfterExit = true;
-            ExecStart = pkgs.writeShellScript "eval-proxy-env" ''
-              [ -f /etc/proxy.env ] || exit 0
-              set -a; source /etc/proxy.env; set +a
-              env | grep -iE '^(http|https|no)_proxy=' > /run/nix-proxy.env
-            '';
-          };
+  den.aspects.bur34u.nixos =
+    { pkgs, ... }:
+    {
+      systemd.services.nix-proxy-env = {
+        wantedBy = [ "nix-daemon.service" ];
+        before = [ "nix-daemon.service" ];
+        path = [
+          pkgs.iproute2
+          pkgs.gawk
+        ];
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          ExecStart = pkgs.writeShellScript "eval-proxy-env" ''
+            [ -f /etc/proxy.env ] || exit 0
+            set -a; source /etc/proxy.env; set +a
+            env | grep -iE '^(http|https|no)_proxy=' > /run/nix-proxy.env
+          '';
         };
-
-        systemd.services.nix-daemon.serviceConfig.EnvironmentFile = "-/run/nix-proxy.env";
-
-        security.sudo.extraConfig = ''
-          Defaults env_keep += "http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY"
-        '';
       };
 
-    provides.to-users.hjem =
-      { ... }:
-      {
-        rum.programs.zsh.initConfig = ''
-          [ -f /run/nix-proxy.env ] && source /run/nix-proxy.env
-        '';
-      };
-  };
+      systemd.services.nix-daemon.serviceConfig.EnvironmentFile = "-/run/nix-proxy.env";
+
+      security.sudo.extraConfig = ''
+        Defaults env_keep += "http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY"
+      '';
+
+      programs.zsh.shellInit = ''
+        [ -f /run/nix-proxy.env ] && source /run/nix-proxy.env
+      '';
+    };
 }
