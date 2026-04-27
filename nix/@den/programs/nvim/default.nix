6959000fb8b23229000fb080806e8e688af613cd
declnix <20567435+declnix@users.noreply.github.com>
2026-04-27 12:41:05 +0000

    refactor(programs): move nvf to nvim directory and split into default and lib file

--- nix/@den/programs/nvim/default.nix
+++ nix/@den/programs/nvim/default.nix
@@ -1,40 +1,8 @@
 {
   den,
-  lib,
-  inputs,
   ...
 }:
 {
-  den.lib.nvim.package =
-    pkgs: vimAspect: ctx:
-    (inputs.nvf.lib.neovimConfiguration {
-      inherit pkgs;
-      modules = [ (den.lib.nvim.module vimAspect ctx) ];
-    }).neovim;
-
-  den.lib.nvim.module =
-    vimAspect: ctx:
-    let
-      vimClass =
-        { class, aspect-chain }:
-        den._.forward {
-          each = lib.singleton true;
-          fromClass = _: "vim";
-          intoClass = _: "nvf";
-          intoPath = _: [ "vim" ];
-          fromAspect = _: lib.head aspect-chain;
-          adaptArgs = lib.id;
-        };
-
-      aspect = den.lib.parametric.fixedTo ctx {
-        includes = [
-          vimClass
-          vimAspect
-        ];
-      };
-    in
-    den.lib.aspects.resolve "nvf" aspect;
-
   den.aspects.nvim = {
     hjem =
       { pkgs, ... }:
