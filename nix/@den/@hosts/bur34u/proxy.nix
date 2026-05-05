5c00d336ecb96c3d16a71db92213629ed4832766
declnix <20567435+declnix@users.noreply.github.com>
2026-05-05 10:58:33 +0200

    refactor(bur34u): use /etc/proxy as EnvironmentFile for nix-daemon

--- nix/@den/@hosts/bur34u/proxy.nix
+++ nix/@den/@hosts/bur34u/proxy.nix
@@ -1,32 +1,22 @@
 { den, ... }:
-let
-  sourceExported = file: "set -a; source ${file}; set +a";
-in
 {
   den.aspects.bur34u = {
     nixos =
       { pkgs, ... }:
       {
         systemd.services.nix-daemon.serviceConfig = {
-          ExecStartPre = pkgs.writeShellScript "gen-proxy-env" ''
-            [ -f /etc/proxy.env ] || exit 0
-            ${sourceExported "/etc/proxy.env"}
-            env | grep -iE '^(http|https|no)_proxy=' > /run/nix-proxy.env || true
-          '';
-          EnvironmentFile = "-/run/nix-proxy.env";
+          EnvironmentFile = "-/etc/proxy";
         };
 
         security.sudo.extraConfig = ''
-          Defaults env_keep += "http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY"
+          Defaults env_keep += "http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY all_proxy ALL_PROXY"
         '';
       };
 
     provides.to-users = {
       zsh = {
         extraConfig = ''
-          if [ -f /run/nix-proxy.env ]; then
-            ${sourceExported "/run/nix-proxy.env"}
-          fi
+          [ -f /etc/proxy ] && source /etc/proxy
         '';
       };
     };
