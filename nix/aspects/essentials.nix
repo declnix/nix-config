9af9e8a0aee6bd96e92a7654aafd36c5f1d5a347
declnix <20567435+declnix@users.noreply.github.com>
2026-05-08 12:57:11 +0200

    feat(essentials): enable bat, eza, fzf, and zoxide

--- nix/aspects/essentials.nix
+++ nix/aspects/essentials.nix
@@ -4,22 +4,22 @@
     hjem =
       { pkgs, ... }:
       {
-        # rum.programs.bat = {
-        #   enable = true;
-        #   integrations.zsh.enable = true;
-        # };
-        # rum.programs.eza = {
-        #   enable = true;
-        #   integrations.zsh.enable = true;
-        # };
-        # rum.programs.fzf = {
-        #   enable = true;
-        #   integrations.zsh.enable = true;
-        # };
-        # rum.programs.zoxide = {
-        #   enable = true;
-        #   integrations.zsh.enable = true;
-        # };
+        rum.programs.bat = {
+          enable = true;
+          integrations.zsh.enable = true;
+        };
+        rum.programs.eza = {
+          enable = true;
+          integrations.zsh.enable = true;
+        };
+        rum.programs.fzf = {
+          enable = true;
+          integrations.zsh.enable = true;
+        };
+        rum.programs.zoxide = {
+          enable = true;
+          integrations.zsh.enable = true;
+        };
       };
 
   };
