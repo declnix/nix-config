2b8d1a3b36ad14fc3c04d6043d8a9a43be6cd732
declnix <20567435+declnix@users.noreply.github.com>
2026-05-13 18:00:52 +0200

    aspects: merge essentials into console

--- nix/configurations/aspects/console/console.nix
+++ nix/configurations/aspects/console/console.nix
@@ -5,5 +5,26 @@
       zsh
       tmux
     ];
+
+    hjem =
+      { pkgs, ... }:
+      {
+        extraRum.programs.bat = {
+          enable = true;
+          integrations.zsh.enable = true;
+        };
+        extraRum.programs.eza = {
+          enable = true;
+          integrations.zsh.enable = true;
+        };
+        rum.programs.fzf = {
+          enable = true;
+          integrations.zsh.enable = true;
+        };
+        rum.programs.zoxide = {
+          enable = true;
+          integrations.zsh.enable = true;
+        };
+      };
   };
 }
