ddab9ee0d257e4576dc2670ea8a36e89fd61c38f
declnix <20567435+declnix@users.noreply.github.com>
2026-05-14 15:43:16 +0200

    lazygit: update rum
    update lazygit module to use rum programs namespace

--- nix/hjemModules/programs/lazygit.nix
+++ nix/hjemModules/programs/lazygit.nix
@@ -8,10 +8,10 @@ let
   inherit (lib.modules) mkIf;
   inherit (lib.options) mkEnableOption mkPackageOption;
 
-  cfg = config.extraRum.programs.lazygit;
+  cfg = config.rum.programs.lazygit;
 in
 {
-  options.extraRum.programs.lazygit = {
+  options.rum.programs.lazygit = {
     enable = mkEnableOption "lazygit";
     package = mkPackageOption pkgs "lazygit" { nullable = true; };
     integrations.zsh.enable = mkEnableOption "lazygit lg alias in zsh";
@@ -20,7 +20,7 @@ in
   config = mkIf cfg.enable {
     packages = mkIf (cfg.package != null) [ cfg.package ];
 
-    zsh.initConfig = mkIf cfg.integrations.zsh.enable ''
+    rum.programs.zsh.initConfig = mkIf cfg.integrations.zsh.enable ''
       alias lg='${lib.meta.getExe cfg.package}'
     '';
   };
