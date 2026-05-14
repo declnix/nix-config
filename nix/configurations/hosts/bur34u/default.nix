517fdec51bcd5cd865252cfb54b11ec0ff99b954
declnix <20567435+declnix@users.noreply.github.com>
2026-05-14 16:06:20 +0200

    bur34u: cleanup wsl
    remove redundant wsl aspect from host includes and fix syntax

--- nix/configurations/hosts/bur34u/default.nix
+++ nix/configurations/hosts/bur34u/default.nix
@@ -2,11 +2,12 @@
 {
   den.aspects.bur34u = {
     includes = with den.aspects; [
-      wsl
       fonts
       podman
     ];
   };
 
-  den.hosts.x86_64-linux.bur34u = { };
+  den.hosts.x86_64-linux.bur34u = {
+    wsl.enable = true;
+  };
 }
