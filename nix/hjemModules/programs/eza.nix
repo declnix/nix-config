14fddbf24144bf9d0622e6aa1cd1d46184a0a618
declnix <20567435+declnix@users.noreply.github.com>
2026-05-14 15:43:09 +0200

    eza: update rum
    update eza module to use rum programs namespace

--- nix/hjemModules/programs/eza.nix
+++ nix/hjemModules/programs/eza.nix
@@ -8,10 +8,10 @@ let
   inherit (lib.modules) mkIf;
   inherit (lib.options) mkEnableOption mkPackageOption;
 
-  cfg = config.extraRum.programs.eza;
+  cfg = config.rum.programs.eza;
 in
 {
-  options.extraRum.programs.eza = {
+  options.rum.programs.eza = {
     enable = mkEnableOption "eza";
     package = mkPackageOption pkgs "eza" { nullable = true; };
     integrations.zsh.enable = mkEnableOption "eza aliases in zsh";
@@ -20,7 +20,7 @@ in
   config = mkIf cfg.enable {
     packages = mkIf (cfg.package != null) [ cfg.package ];
 
-    zsh.initConfig = mkIf cfg.integrations.zsh.enable ''
+    rum.programs.zsh.initConfig = mkIf cfg.integrations.zsh.enable ''
       alias ls='${lib.meta.getExe cfg.package} --icons'
       alias ll='${lib.meta.getExe cfg.package} -l --icons --git'
       alias la='${lib.meta.getExe cfg.package} -la --icons --git'
