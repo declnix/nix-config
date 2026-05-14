60cf3faf09cfb9138245ab170d5ef94558e2a868
declnix <20567435+declnix@users.noreply.github.com>
2026-05-14 17:46:57 +0200

    hjem: automate disabledModules generation

--- nix/configurations/hjem.nix
+++ nix/configurations/hjem.nix
@@ -4,21 +4,46 @@
   lib,
   ...
 }:
+let
+  # Automatyczne wyłączanie modułów z hjem-rum, które mamy zdefiniowane lokalnie
+  # Skanujemy folder ../hjemModules i mapujemy pliki na strukturę w hjem-rum/modules/
+  disabledFromRum =
+    let
+      hjemModulesDir = ../hjemModules;
+      
+      # Rekurencyjne zbieranie plików .nix
+      getFiles = dir:
+        lib.mapAttrsToList (name: type:
+          let path = dir + "/${name}"; in
+          if type == "directory" then getFiles path
+          else if lib.hasSuffix ".nix" name then [ path ]
+          else [ ]
+        ) (builtins.readDir dir);
+      
+      allLocalFiles = lib.flatten (getFiles hjemModulesDir);
+
+      toRumPath = p:
+        let
+          relPath = lib.removePrefix (toString hjemModulesDir + "/") (toString p);
+        in
+        "${inputs.hjem-rum}/modules/${relPath}";
+    in
+    builtins.filter builtins.pathExists (map toRumPath allLocalFiles);
+in
 {
-  den.default.nixos.hjem.extraModules = [
-    {
-      _module.args.dag = inputs.dag.lib { inherit lib; };
-      _module.args.inputs = inputs;
-      disabledModules = [
-        "${inputs.hjem-rum}/modules/collection/programs/zsh.nix"
-      ];
-    }
-  ]
-  ++ [
-    inputs.hjem-impure.hjemModules.default
-    inputs.hjem-rum.hjemModules.default
-  ]
-  ++ (inputs.import-tree ../hjemModules).imports;
+  den.default.nixos.hjem.extraModules =
+    [
+      {
+        _module.args.dag = inputs.dag.lib { inherit lib; };
+        _module.args.inputs = inputs;
+        disabledModules = disabledFromRum;
+      }
+    ]
+    ++ [
+      inputs.hjem-impure.hjemModules.default
+      inputs.hjem-rum.hjemModules.default
+    ]
+    ++ (inputs.import-tree ../hjemModules).imports;
   den.default.hjem = {
     impure.enable = true;
   };
