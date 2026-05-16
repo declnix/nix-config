fca9091076930e004d5021d80ead5de447822718
declnix <20567435+declnix@users.noreply.github.com>
2026-05-16 14:46:14 +0200

    podman: configure rootless socket for docker-compose compatibility

--- modules/podman.nix
+++ modules/podman.nix
@@ -10,6 +10,7 @@
             enable = true;
             dockerCompat = true;
             defaultNetwork.settings.dns_enabled = true;
+            autoPrune.enable = true;
           };
           oci-containers.backend = "podman";
         };
@@ -17,7 +18,10 @@
           docker-compose
           fuse-overlayfs
         ];
-        environment.variables.PODMAN_IGNORE_CGROUPSV1_WARNING = "1";
+        environment.variables = {
+          PODMAN_IGNORE_CGROUPSV1_WARNING = "1";
+          DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
+        };
       };
   };
 }
