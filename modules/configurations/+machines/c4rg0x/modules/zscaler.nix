8534afc85e3a736b07d1c9b0c1fac56dc3d88fce
declnix <20567435+declnix@users.noreply.github.com>
2026-06-30 12:29:17 +0000

    c4rg0x: zscaler -> fix ssl falling on nvim plugin

--- modules/configurations/+machines/c4rg0x/modules/zscaler.nix
+++ modules/configurations/+machines/c4rg0x/modules/zscaler.nix
@@ -1,10 +1,18 @@
 { ... }:
 {
-  den.aspects.c4rg0x.provides.nixos-user.nixos =
-    { pkgs, ... }:
+  den.aspects.c4rg0x.nixos =
+    { lib, pkgs, ... }:
     {
       security.pki.certificateFiles = [
         "${pkgs.zscaler-cacert}/etc/ssl/certs/zscaler-ca.crt"
+      ];
+
+      nix.settings.ssl-cert-file =
+        "/etc/ssl/certs/ca-certificates.crt";
+
+      systemd.services.nix-daemon.serviceConfig.Environment = lib.mkAfter [
+        "NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt"
+        "CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt"
       ];
     };
 }
