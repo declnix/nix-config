ec30f3039ea1b331e90a861140357fe7654f4a67
declnix <20567435+declnix@users.noreply.github.com>
2026-07-02 09:47:45 +0000

    bur34u: fix zsh proxy

--- modules/configurations/+machines/bur34u/modules/proxy.nix
+++ modules/configurations/+machines/bur34u/modules/proxy.nix
@@ -4,12 +4,19 @@
       systemd.services.nix-daemon.serviceConfig.EnvironmentFile = "-/etc/proxy";
 
       security.sudo.extraConfig = ''
-        Defaults env_keep += "http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY"
+        Defaults env_keep += "${lib.concatStringsSep " " [
+          "http_proxy"
+          "https_proxy"
+          "no_proxy"
+          "HTTP_PROXY"
+          "HTTPS_PROXY"
+          "NO_PROXY"
+        ]}"
       '';
     };
 
     provides.to-users = {
-      zef = {
+      zsh = {
         initExtra = ''
           if [ -f /etc/proxy ]; then
             set -a; source /etc/proxy; set +a
