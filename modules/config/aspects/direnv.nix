c3491f5a72bbef12c7d17adc83f9066501942f3b
declnix <20567435+declnix@users.noreply.github.com>
2026-07-11 00:50:23 +0200

    direnv: reserve local development ports

--- modules/config/aspects/direnv.nix
+++ modules/config/aspects/direnv.nix
@@ -8,5 +8,10 @@
           integrations.zsh.enable = true;
         };
       };
+
+    # Reserve ports for local development services.
+    nixos = {
+      boot.kernel.sysctl."net.ipv4.ip_local_reserved_ports" = "61000-64999";
+    };
   };
 }
