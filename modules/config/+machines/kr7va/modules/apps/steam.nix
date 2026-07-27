462a23426912605f6d1bf5ac1a8f210c35d629c1
declnix <20567435+declnix@users.noreply.github.com>
2026-07-27 20:15:33 +0200

    kr7va: steam -> add gaming setup

---
+++ modules/config/+machines/kr7va/modules/apps/steam.nix
@@ -0,0 +1,37 @@
+{ den, ... }:
+{
+  den.aspects.kr7va = {
+    provides.declnix.nixos = { pkgs, ... }: {
+      programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
+        gamescopeSession.enable = true;
+        protontricks.enable = true;
+        extraCompatPackages = with pkgs; [
+          proton-ge-bin
+        ];
+        extraPackages = with pkgs; [
+          mangohud
+        ];
+      };
+
+      programs.gamemode.enable = true;
+      programs.gamescope.enable = true;
+
+      environment.systemPackages = with pkgs; [
+        mangohud
+        protonup-qt
+      ];
+    };
+
+    includes = [
+      (den.batteries.unfree [
+        "steam"
+        "steam-original"
+        "steam-run"
+        "steam-unwrapped"
+      ])
+    ];
+  };
+}
