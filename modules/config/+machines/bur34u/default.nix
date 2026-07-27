5851efeb260e777cdbf5ef575577fd08c5a6ec1a
declnix <20567435+declnix@users.noreply.github.com>
2026-07-27 14:19:15 +0200

    bur34u: nvim -> isolate jdtls workspace

--- modules/config/+machines/bur34u/default.nix
+++ modules/config/+machines/bur34u/default.nix
@@ -5,7 +5,7 @@
 {
   den.aspects.bur34u = {
     provides.nixos-user = { user, ... }: {
-      nvim = { ... }: lib.mkMerge [
+      nvim = { pkgs, ... }: lib.mkMerge [
         {
           # frontend
           lsp.presets = {
@@ -50,6 +50,32 @@
         }
         {
           # backend
+          lsp.servers.jdt-language-server.cmd = lib.mkForce (lib.generators.mkLuaInline ''
+            (function()
+              local root = vim.fs.root(0, {
+                '.git',
+                'build.gradle',
+                'build.gradle.kts',
+                'build.xml',
+                'pom.xml',
+                'settings.gradle',
+                'settings.gradle.kts',
+              }) or vim.fn.getcwd()
+              local name = vim.fn.fnamemodify(root, ':t')
+              local workspace = name .. '-' .. string.sub(vim.fn.sha256(root), 1, 12)
+              return {
+                '${lib.getExe pkgs.jdt-language-server}',
+                '-configuration',
+                vim.fn.stdpath('cache') .. '/jdtls/config',
+                '-data',
+                vim.fn.stdpath('cache') .. '/jdtls/workspace/' .. workspace,
+                unpack(vim.tbl_map(function(arg)
+                  return '--jvm-arg=' .. arg
+                end, vim.split(os.getenv('JDTLS_JVM_ARGS') or "", '%s+', { trimempty = true }))),
+              }
+            end)()
+          '');
+
           languages = {
             java = {
               enable = true;
