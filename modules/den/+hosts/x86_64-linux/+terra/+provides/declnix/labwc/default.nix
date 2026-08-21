210173b1227f0cb8362d2a88bc59c4340ecd2257
declnix <20567435+declnix@users.noreply.github.com>
2026-08-21 22:35:48 +0200

    terra: labwc -> split host and user reload

--- modules/den/+hosts/x86_64-linux/+terra/+provides/declnix/labwc/default.nix
+++ modules/den/+hosts/x86_64-linux/+terra/+provides/declnix/labwc/default.nix
@@ -2,37 +2,22 @@
 {
   den.aspects.terra = {
     provides.declnix = {
-      hjem = {
+      hjem = { config, pkgs, ... }: {
         files.".config/labwc/rc.xml".source = ./rc.xml;
-      };
-
-      nixos = { pkgs, ... }: {
-        programs.labwc.enable = true;
-
-        services.greetd = {
-          enable = true;
-          settings.default_session = {
-            command = "${pkgs.labwc}/bin/labwc";
-            user = "declnix";
-          };
-        };
 
         systemd.services.labwc-reload = {
           description = "Reload labwc after Hjem file activation";
-          wantedBy = [ "hjem.target" ];
-          after = [ "hjem-reload@declnix.service" ];
-          restartTriggers = [
-            ./rc.xml
+          wantedBy = [ "default.target" ];
+          reloadTriggers = [
+            config.files.".config/labwc/rc.xml".source
           ];
 
           serviceConfig = {
             Type = "oneshot";
-            User = "declnix";
+            RemainAfterExit = true;
+            ExecStart = "${pkgs.coreutils}/bin/true";
+            ExecReload = "${pkgs.bash}/bin/bash -lc '${pkgs.procps}/bin/pkill -HUP -u \"$USER\" -x labwc || true'";
           };
-
-          script = ''
-            ${pkgs.procps}/bin/pkill -HUP -x labwc || true
-          '';
         };
       };
     };
---
+++ modules/den/+hosts/x86_64-linux/+terra/labwc.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  den.aspects.terra = {
+    nixos = { pkgs, ... }: {
+      programs.labwc.enable = true;
+
+      services.greetd = {
+        enable = true;
+        settings.default_session = {
+          command = "${pkgs.labwc}/bin/labwc";
+          user = "declnix";
+        };
+      };
+    };
+  };
+}
